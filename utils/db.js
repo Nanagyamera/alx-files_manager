59a4bf472798190ee07cb8ce06119246629bff18
Nanagyamera <nanagyamera3@gmail.com>
2023-09-02 08:36:49 -0700

    Add utility classes and functions in the 'utils' directory

--- utils/db.js
+++ utils/db.js
@@ -1,41 +1,55 @@
 const { MongoClient } = require('mongodb');
 
+// Configuration options for the MongoDB connection
+const mongoOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Create a MongoDB client
 class DBClient {
-    constructor() {
-        const dbHost = process.env.DB_HOST || 'localhost';
-        const dbPort = process.env.DB_PORT || 27017;
-        const dbName = process.env.DB_DATABASE || 'files_manager';
+  constructor() {
+    // MongoDB connection URL
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
 
-        const uri = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+    this.client = new MongoClient(`mongodb://${dbHost}:${dbPort}`, mongoOptions);
 
-        this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    // Connect to MongoDB
+    this.client.connect()
+      .then(() => console.log('Connected to MongoDB'))
+      .catch((error) => console.error('MongoDB connection error:', error));
+  }
 
-        this.isAlive = false;
+  /**
+   * Check if the connection to MongoDB is alive.
+   * @returns {boolean} - True if the connection is successful, false otherwise.
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-        this.connect();
-    }
+  /**
+   * Get the number of documents in the 'users' collection.
+   * @returns {Promise<number>} - The number of user documents in the collection.
+   */
+  async nbUsers() {
+    const usersCollection = this.client.db().collection('users');
+    return usersCollection.countDocuments();
+  }
 
-    async connect() {
-        try {
-            await this.client.connect();
-            this.isAlive = true;
-            console.log('Connected to MongoDB');
-        } catch (error) {
-            console.error('MongoDB Connection Error:', error);
-        }
-    }
-
-    async nbUsers() {
-        const usersCollection = this.client.db().collection('users');
-        return usersCollection.countDocuments();
-    }
-
-    async nbFiles() {
-        const filesCollection = this.client.db().collection('files');
-        return filesCollection.countDocuments();
-    }
+  /**
+   * Get the number of documents in the 'files' collection.
+   * @returns {Promise<number>} - The number of file documents in the collection.
+   */
+  async nbFiles() {
+    const filesCollection = this.client.db().collection('files');
+    return filesCollection.countDocuments();
+  }
 }
 
+// Create and export an instance of DBClient
 const dbClient = new DBClient();
 
 module.exports = dbClient;
